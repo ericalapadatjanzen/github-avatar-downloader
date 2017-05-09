f73948aad5f7ef193d2011539947a74cbcf9fd98
Erica Lapadat-Janzen <ericalapadatjanzen@gmail.com>
2017-05-09 01:34:36 +0000

    created getRepoContributors function and the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
+
+
+var https = require('https');
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+var GITHUB_USER = "ericalapadatjanzen";
+var GITHUB_TOKEN = "16ef1493f4eb677a3a7c1e1d7aacf2619b812618";
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log("requestURL", request.get(requestURL));
+
+}
+getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
