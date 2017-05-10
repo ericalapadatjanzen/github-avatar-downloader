8f209a2259972ab68830d79aeb7ddb3b16e2ed65
Erica Lapadat-Janzen <ericalapadatjanzen@gmail.com>
2017-05-10 02:00:44 +0000

    refractored download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -10,30 +10,38 @@ var fs = require('fs');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  if (!repoOwner | !repoName){
+  if (!repoOwner || !repoName){
     console.log("ERROR! You did not enter a valid repo owner or repo name.");
+  } else {
+    var options =  {
+      url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+      headers: {
+        'User-Agent': 'ericalapadatjanzen'
+      }
+    };
+
+    request(options, cb);
   }
-  var options =  {
-    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
-    headers: {
-      'User-Agent': 'ericalapadatjanzen'
-    }
-  };
-
-  request(options, cb);
-
 }
 
-function downloadingImageByURL(url, filepath){
+function downloadingImageByURL(url, filepath) {
+  console.log('Downloading avatar', url, 'to:', filepath);
   request(url).pipe(fs.createWriteStream(filepath));
-
 }
 getRepoContributors(process.argv[2], process.argv[3], function(err, result, body) {
-
-  let parsedResults = JSON.parse(body);
-  for (var i = 0 ; i < parsedResults.length; i++) {
-    let avatarUrl = parsedResults[i]['avatar_url'];
-    downloadingImageByURL(avatarUrl, `avatars/${i}.jpg`);
+  // console.log(arguments);
+  if (err || result.statusCode !== 200) {
+    console.log('Error retrieving avatars:', err);
+    console.log('HTTP status code:', result.statusCode);
+    console.log('Error message:', result.statusMessage);
+  } else {
+    let parsedResults = JSON.parse(body);
+    // console.log('parseResults:', parsedResults);
+    for (var i = 0 ; i < parsedResults.length; i++) {
+      let entry = parsedResults[i];
+      let avatarUrl = entry['avatar_url'];
+      downloadingImageByURL(avatarUrl, `avatars/${entry.login}.jpg`);
+    }
   }
 });
 
